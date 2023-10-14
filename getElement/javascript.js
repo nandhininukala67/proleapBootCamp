74912f962bcc723787011d6a54e3cd57e0bfff74
nandhininukala67 <119854682+nandhininukala67@users.noreply.github.com>
2023-10-14 10:31:59 +0530

    get elements

---
+++ getElement/javascript.js
@@ -0,0 +1,7 @@
+//document.body.innerHTML = "<h1>this is change context </h1>";
+
+//document.getElementById("id2").innerHTML="using id";
+
+document.getElementsByTagName("h1")[0].innerHTML="hello java";
+
+//document.getElementsByClassName("class1")[0].innerHTML="using class";
