60f34850fc750c37d1b14203cddb73651c0fa25b
nandhininukala67 <sivanandhini@prodevans.com>
2023-10-26 18:02:44 +0530

    React project

---
+++ src/pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      UserName:
+      <input type="text" placeholder="Username"/><br />
+      Password:
+      <input type="text" placeholder="Password"/><br />
+      <button>Submit</button>
+    </div>
+  )
+}
+
+export default Login;
